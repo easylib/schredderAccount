41ca6be2cb668e876bcdbc4cd66a38533c280688
kruemel <soeren@siegismund-poschmann.de>
2013-10-28 12:59:55 +0100

    Stuff

--- Easy/schredderAccount/Account.php
+++ Easy/schredderAccount/Account.php
@@ -7,18 +7,18 @@ class Account
 	private $key;
 	public function __construct()
 	{
-		$this->c = new Easy\Curl\Curl();
+		$this->c = new \Easy\Curl\Curl();
 		$this->c->addCookie("Client", "PHP");
 	}
 	public function setLoginSession($session)
 	{
 		$this->loginSession = $session;
-		$this->c->addCookie("appSession", $session);
+		$this->c->addCookie("loginSession", $session);
 	}
 	public function setAppSession($session)
 	{
 		$this->appSession = $session;
-		$this->c->addCookie("loginSession", $session);
+		$this->c->addCookie("appSession", $session);
 	}
 	public function appLogin($key, $secret, $save = true)
 	{
@@ -38,18 +38,37 @@ class Account
 	}
 	public function getLoginSession($callback = NULL)
 	{
-		return "http://account.schredder.pw/app?key=".$key."&callback=".urlencode($callback);
+		if($callback==NULL)
+		{
+			$callback="http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+		}
+		return "http://account.schredder.pw/app?key=".$this->key."&callback=".urlencode($callback);
 	}
 	public function isLogin()
 	{
 		$r = $this->c->get("http://account.schredder.pw/api/user/login/");
+		var_dump($r);
 		#$r = file_get_contents("http://account.schredder.pw/api/loginSession?key=".base64_encode($this->loginSession));
         $d = json_decode($r, true);
         if($d["status"]==false)
         {
-        	return true;
+        	return false;
         }
+        $this->id = $d["id"];
         return $d["login"];
 	}
+	public function checkRight($right, $userID=NULL)
+	{
+		if($userID==NULL)
+		{
+			$userID==$this->id;
+		}
+		$r = $this->c->get("http://account.schredder.pw/api/user/userRights/?userID=".$userID."&right=".$right);
+		return $r[$right];
+	}
+	/*public function test()
+	{
+		return $this->c->get("http://account.schredder.pw/api/app/testSession");
+	}*/
 }
 ?>
