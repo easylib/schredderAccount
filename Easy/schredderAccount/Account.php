7f84eb1802846c8c0756e1a2e0c0fcac8504ab3a
kruemel <soeren@siegismund-poschmann.de>
2013-10-28 13:12:52 +0100

    Stuff

--- Easy/schredderAccount/Account.php
+++ Easy/schredderAccount/Account.php
@@ -55,7 +55,7 @@ class Account
         	return false;
         }
         $this->id = $d["id"];
-        if($d["login"]==true)
+        if($d["login"]=="true")
         {
         	return true;
         }
